389e422f4f17493764080f1fb2703049759f5af2
Jaime Cueva Sánchez <jaimcuev@gmail.com>
2024-07-26 14:19:52 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -15,3 +15,22 @@ module "cluster" {
 
   depends_on = [module.networking]
 }
+
+module "odf" {
+  source = "./modules/instance"
+  
+  project = var.project
+  environment = var.environment
+  ibm_region = var.ibm_region
+  resource_group = var.resource_group
+  vpc_id = module.networking.vpc_id
+  vpc_subnet_id = module.networking.vpc_subnet_id
+  cluster_id = module.cluster.cluster_id
+  zone = var.zone
+  flavor = "bx2.16x64"
+  worker_count = 3
+  entitlement_wp = var.entitlement_wp
+  worker_pool_name = "odf"
+
+  depends_on = [module.networking, module.cluster]
+}
